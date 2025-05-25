de16fedf86937018f5bbb6c99a4a7c4c1dc05f48
winlogon <walker84837@gmail.com>
2025-05-25 20:51:05 +0200

    fix: check database type correctly

--- src/main/java/org/winlogon/homemanager/HomeManagerPlugin.java
+++ src/main/java/org/winlogon/homemanager/HomeManagerPlugin.java
@@ -1,5 +1,8 @@
 package org.winlogon.homemanager;
 
+import java.util.logging.Logger;
+
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import org.winlogon.homemanager.database.SQLiteHandler;
@@ -8,6 +11,8 @@ import org.winlogon.homemanager.database.AdvancedDatabaseConfig;
 
 public class HomeManagerPlugin extends JavaPlugin {
     private DataHandler databaseHandler;
+    FileConfiguration config;
+    Logger logger;
 
     @Override
     public void onEnable() {
@@ -20,26 +25,10 @@ public class HomeManagerPlugin extends JavaPlugin {
             throw new RuntimeException("PostgreSQL JDBC Driver not found.");
         }
 
-
-        var config = getConfig();
-        var logger = getLogger();
+        this.config = getConfig();
+        this.logger = getLogger();
         var postgresEnabled = config.getBoolean("postgres.enabled", false);
-        if (!postgresEnabled) {
-            databaseHandler = new SQLiteHandler(getDataFolder());
-            logger.info("Using SQLite for home storage.");
-        }
-
-        var databaseConfig = new AdvancedDatabaseConfig(
-            postgresEnabled,
-            config.getString("postgres.host", "localhost"),
-            config.getInt("postgres.port", 5432),
-            config.getString("postgres.database", "minecraft"),
-            config.getString("postgres.user", "postgres"),
-            config.getString("postgres.password", "")
-        );
-
-        databaseHandler = new PostgresHandler(databaseConfig, logger);
-        getLogger().info("Using PostgreSQL for home storage.");
+        handleDatabaseType(postgresEnabled);
 
         CommandHandler<DataHandler> commandHandler = new CommandHandler<>(databaseHandler);
         commandHandler.registerCommands();
@@ -50,6 +39,26 @@ public class HomeManagerPlugin extends JavaPlugin {
         if (databaseHandler != null) {
             databaseHandler.closeConnection();
         }
+    }
+
+    private void handleDatabaseType(boolean isPostgres) {
+        if (!isPostgres) {
+            databaseHandler = new SQLiteHandler(getDataFolder());
+            logger.info("Using SQLite for home storage.");
+            return;
+        }
+
+        var databaseConfig = new AdvancedDatabaseConfig(
+            isPostgres,
+            config.getString("postgres.host", "localhost"),
+            config.getInt("postgres.port", 5432),
+            config.getString("postgres.database", "minecraft"),
+            config.getString("postgres.user", "postgres"),
+            config.getString("postgres.password", "")
+        );
+    
+        databaseHandler = new PostgresHandler(databaseConfig, logger);
+        getLogger().info("Using PostgreSQL for home storage.");
     }
 }
 
